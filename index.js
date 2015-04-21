f2f4abc999d2c9182bec5beba756a1eacab20e95
Fred Spieler <fred@fredspieler.com>
2015-04-21 00:25:24 -0400

    Created trie from dict and some minor cleanup

--- index.js
+++ index.js
@@ -1,19 +1,77 @@
-letterController = function(){
-    letters = ['e','t','a','o','i','n','s','h','r','d','l','c','u','m','w','f','g','y','p','b','v','k','j','x','q','z'];
-    index = 0;
-    function setLetter(l){
-        letter = l;
-        $("#letter").text(l);
+function Trie(letters){
+    this.nextTries = {};
+    this.count = 1;
+    if(_.isUndefined(letters) || letters.length === 0){
+        this.isFinal = true;
+    } else {
+        var head = _.head(letters);
+        var tail = _.tail(letters);
+        this.isFinal = false;
+        this.nextTries[head] = new Trie(tail);
     }
-    setLetter(letters[0]);
+}
+
+Trie.prototype.pushString = function(string){
+    var c = string.charAt(0)
+    this.nextTries.push(letter);
+    this.nextTries.sort();
+    this.count += 1;
+}
+
+Trie.prototype.getTrie = function(l){
+    return nextTries[l];
+}
+
+Trie.prototype.nextLetters = function(){
+    return _.keys(nextTries).sort();
+};
+
+Trie.prototype.construct = function(chars){
+    if(chars.length === 0){
+        var isFinal = true;
+        return;
+    }
+    var key = _.head(chars);
+    var tail = _.tail(chars);
+    this.count += 1;
+    if(key in this.nextTries){
+        this.nextTries[key].construct(tail);
+    } else{
+        this.nextTries[key] = new Trie(tail);
+    }
+}
+
+function populateTrie(){
+    var retTrie = new Trie();
+    for(var word in dict){
+        retTrie.construct(dict[word].toLowerCase().split(""));
+    }
+    return retTrie;
+}
+
+rootTrie = populateTrie();
+
+wordFinder = function(){
+
+}();
+
+letterController = function(){
+    var letters = ['e','t','a','o','i','n','s','h','r','d','l','c','u','m','w','f','g','y','p','b','v','k','j','x','q','z'];
+    var index = 0;
+    function setIndex(i){
+        index = i;
+        var letter = letters[i];
+        $("#letter").text(letter);
+    }
+    setIndex(0);
     return {
         reset : function(){
-            setLetter(letters[0]);
+            setIndex(0);
             $("#message").empty();
         },
         acceptCurrent : function(){
             $("#message").append(letters[index]);
-            setLetter(letters[0]);
+            setIndex(0);
         },
         resetFlag : false,
         increment : function(){
@@ -22,7 +80,7 @@ letterController = function(){
             } else{
                 index = 0;
             }
-            setLetter(letters[index]);
+            setIndex(index);
         }
     };
 }()
