f3e29767ab61ba79393193fbfd2342509dffc3c9
Fred Spieler <fspieler@gmail.com>
2015-04-27 09:01:49 -0400

    Added 'smart word' detection

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/* {  Trie defs */
 function Trie(letters){
     this.nextTries = {};
     this.count = 1;
@@ -8,22 +9,16 @@ function Trie(letters){
         var tail = _.tail(letters);
         this.isFinal = false;
         this.nextTries[head] = new Trie(tail);
+        this['_'+head] = this.nextTries[head];
     }
 }
 
-Trie.prototype.pushString = function(string){
-    var c = string.charAt(0)
-    this.nextTries.push(letter);
-    this.nextTries.sort();
-    this.count += 1;
-}
-
 Trie.prototype.getTrie = function(l){
-    return nextTries[l];
+    return this.nextTries[l];
 }
 
 Trie.prototype.nextLetters = function(){
-    return _.keys(nextTries).sort();
+    return _.keys(this.nextTries).sort();
 };
 
 Trie.prototype.construct = function(chars){
@@ -36,69 +31,151 @@ Trie.prototype.construct = function(chars){
     this.count += 1;
     if(key in this.nextTries){
         this.nextTries[key].construct(tail);
+        this['_'+key] = this.nextTries[key];
     } else{
         this.nextTries[key] = new Trie(tail);
+        this['_'+key] = this.nextTries[key];
     }
+}
+
+Trie.prototype.suffixes = function(){
+    function recursiveHelper(trie,str,solns){
+        if(trie.isFinal){
+            solns.push(str);
+        }
+        for(var nl in trie.nextTries){
+            recursiveHelper(trie.nextTries[nl],str + nl,solns);
+        }
+    }
+    var s = "";
+    var retArr = [];
+    recursiveHelper(this,s, retArr);
+    return retArr;
 }
 
 function populateTrie(){
     var retTrie = new Trie();
+    retTrie.isFinal = false;
     for(var word in dict){
         retTrie.construct(dict[word].toLowerCase().split(""));
     }
     return retTrie;
 }
 
+/* }  end Trie defs */
+
 rootTrie = populateTrie();
 
-wordFinder = function(){
+/* {  NaiveLetterProvider defs */
 
-}();
+function NaiveLetterProvider() {}
 
-letterController = function(){
-    var letters = ['e','t','a','o','i','n','s','h','r','d','l','c','u','m','w','f','g','y','p','b','v','k','j','x','q','z'];
-    var index = 0;
-    function setIndex(i){
-        index = i;
-        var letter = letters[i];
-        $("#letter").text(letter);
+NaiveLetterProvider.prototype.incrementLetter = function(){}
+NaiveLetterProvider.prototype.acceptCurrentLetter = function(){}
+
+
+/* }  end NaiveLetterProvider defs */
+
+/* {  SmartWordLetterProvider defs */
+
+function SmartWordLetterProvider(rootTrie) {
+    this.root = rootTrie;
+    this.resetWord();
+    this.resetFlag = false;
+    this.lettersIndex = 0;
+    this.newTrie(this.root);
+}
+
+/* INTERFACE METHODS { */
+SmartWordLetterProvider.prototype.incrementLetter = function(){
+    if(this.lettersIndex < this.letters.length){
+        this.lettersIndex += 1;
+    } else{
+        this.lettersIndex = 0;
     }
-    setIndex(0);
-    return {
-        reset : function(){
-            setIndex(0);
-            $("#message").empty();
-        },
-        acceptCurrent : function(){
-            $("#message").append(letters[index]);
-            setIndex(0);
-        },
-        resetFlag : false,
-        increment : function(){
-            if(index < letters.length){
-                index += 1;
-            } else{
-                index = 0;
-            }
-            setIndex(index);
+    this.displayCurrentLetter();
+}
+SmartWordLetterProvider.prototype.acceptCurrentLetter = function(){
+    this.acceptLetter(this.letters[this.lettersIndex]);
+}
+/* END INTERFACE METHODS } */
+
+/* IMPLEMENTATION METHODS { */
+
+SmartWordLetterProvider.prototype.CANCEL_MESSAGE = "cancel word";
+SmartWordLetterProvider.prototype.ACCEPT_MESSAGE = "accept word";
+SmartWordLetterProvider.prototype.displayCurrentLetter = function(){
+    $("#letter").text(this.letters[this.lettersIndex]);
+}
+SmartWordLetterProvider.prototype.resetWord = function(){
+    this.word = "";
+    $("#word").empty();
+    this.resetFlag = true;
+    this.newTrie(this.root);
+}
+SmartWordLetterProvider.prototype.newTrie = function(trie){
+    this.currentTrie = trie;
+    this.letters = this.currentTrie.nextLetters();
+    this.lettersIndex = 0;
+    if(this.letters.length === 0){
+        this.acceptWord();
+        return;
+    }
+    console.log(this.letters);
+    if(this.letters.length === 1){
+        for(var letter in this.letters){
+            this.acceptLetter(this.letters[letter]);
+            return;
         }
-    };
-}()
+    }
+    if(this.letters.length === 0){
+        this.acceptWord();
+        return;
+    }
+    if(this.word.length > 0){
+        this.letters = _.flatten([this.CANCEL_MESSAGE, this.letters]);
+    }
+    this.isPossibleFinal = this.currentTrie.isFinal;
+    if(this.isPossibleFinal){
+        this.letters = _.flatten([this.ACCEPT_MESSAGE, this.letters]);
+    }
+    this.displayCurrentLetter();
+}
+SmartWordLetterProvider.prototype.acceptLetter = function(letter){
+    if(letter === this.CANCEL_MESSAGE){
+        this.resetWord();
+    } else if(letter === this.ACCEPT_MESSAGE){
+        this.acceptWord();
+    } else{
+        $("#word").append(letter);
+        this.newTrie(this.currentTrie.nextTries[letter]);
+    }
+}
+SmartWordLetterProvider.prototype.acceptWord = function(){
+    $("#message").append( " " + $("#word").text());
+    this.resetWord();
+}
+/* END IMPLEMENTATION METHODS } */
+
+
+/* }  end SmartWordLetterProvider defs */
+
 
 $( document ).ready(function(){
 
     var TIMEOUT_DELAY_MS = 2000;
-    letterController.reset();
+
+    swLetterProvider = new SmartWordLetterProvider(rootTrie);
     setInterval(function(){
-        if(letterController.resetFlag === true){
-            letterController.resetFlag = false;
+        if(swLetterProvider.resetFlag === true){
+            swLetterProvider.resetFlag = false;
             setTimeout(function(){},TIMEOUT_DELAY_MS);
         }
-        letterController.increment();
+        swLetterProvider.incrementLetter();
 
     }, TIMEOUT_DELAY_MS);
 });
 
 $(document).keypress(function(e){
-    letterController.acceptCurrent();
+    swLetterProvider.acceptCurrentLetter();
 });
